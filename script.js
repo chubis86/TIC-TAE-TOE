a2b099375ac63d361d3b2306542af6798bda3453
EFREN NARVAEZ <efrennm86@gmail.com>
2023-09-16 23:08:10 -0600

    GUARDA LOS VALORES DE LOS JUGADORES EN EL ARREGLO

--- script.js
+++ script.js
@@ -1,50 +1,102 @@
-//Arreglo base 
-const arreglo =[
+/////////Arreglo base 
+let arreglo =[
     ['','',''],
     ['','',''],
     ['','','']
 ]; 
 
-//Arreglo con las combinaciones posibles para ganar usando al arrelgo base
-const arregloGanador=[
+/////////Arreglo con las combinaciones posibles para ganar usando al arrelgo base
+let arregloGanador=[
     //filas
-    [arreglo[0][0], arreglo[0][1], arreglo[0][2]],
-    [arreglo[1][0], arreglo[1][1], arreglo[1][2]],
-    [arreglo[2][0], arreglo[2][1], arreglo[2][2]],
+    [[0, 0], [0, 1], [0, 2]],
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
 
     //columnas
-    [arreglo[0][0], arreglo[1][0], arreglo[2][0]],
-    [arreglo[0][1], arreglo[1][1], arreglo[2][1]],
-    [arreglo[0][2], arreglo[1][2], arreglo[2][2]],
+    [[0, 0], [1, 0], [2, 0]],
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]],
 
     //cruzado
-    [arreglo[0][0], arreglo[1][1], arreglo[2][2]],
-    [arreglo[0][2], arreglo[1][1], arreglo[2][0]]
+    [[0, 0], [1, 1], [2, 2]],
+    [[0, 2], [1, 1], [2, 0]]
 ];
 
 
-const casilla1=document.querySelector("#casilla1");
-const casilla2=document.querySelector("#casilla2");
-const casilla3=document.querySelector("#casilla3");
-const casilla4=document.querySelector("#casilla4");
-const casilla5=document.querySelector("#casilla5");
-const casilla6=document.querySelector("#casilla6");
-const casilla7=document.querySelector("#casilla7");
-const casilla8=document.querySelector("#casilla8");
-const casilla9=document.querySelector("#casilla9");
-
-
-//Variable para saber qué jugador va
+/////////Variable para saber qué jugador va
 //Si el jugador es "0" se marcará una x, de otro modo sera un círculo
 let jugador1=true;
 
-//Agregamos escucha de eventos a cada casilla
+
+
+/////////Relacionar el arreglo con las casillas
+function asignarCasilla(){
+    let casilla=document.querySelectorAll(".casilla");
+    let x=0;
+    let y=0;
+    for(let i=0; i<casilla.length; i++){
+        
+        if(y==3){
+            x++;
+            y=0;
+        }
+        //console.log('x:'+x+' y:'+y+' '+casilla[i]);
+        arreglo[x][y]=casilla[i].innerText;
+        y++;    
+    }
+    console.log(arreglo);
+
+}
+
+asignarCasilla();
+/////////Agregamos escucha de eventos a cada casilla
 
 document.querySelectorAll('.casilla').forEach(casilla =>{
     casilla.addEventListener('click', e=>{
-        //Imprimimos si es X o O 
-        console.log(jugador1? 'X' : 'O');
-        //Cambiamos de jugador
-        jugador1=!jugador1;
+        
+        //Checamos primero si ya tiene contenido la casilla
+        if(casilla.textContent=='X'||casilla.textContent=='O'){
+            
+        }else{
+
+            //Imprimimos si es X o O 
+            console.log(jugador1? 'X' : 'O');
+            casilla.innerHTML=`<span class='valor'>${jugador1? 'X' : 'O'}</span>`;
+            console.log(arreglo);
+            asignarCasilla();
+            //Revisar si alguien ha ganado
+            if(juegoGanado()){
+                console.log("ALguien ha ganado");
+            }else{
+                
+                //Cambiamos de jugador
+                jugador1=!jugador1;
+            
+            
+                //Si todas las casillas están llenas y nadie ha ganado entonces declarar un empate y mostrar botón para reiniciar
+            }
+            
+        }
+
+    console.log(arreglo);       
+               
+        
+
     });
 });
+
+function juegoGanado(){
+   let bandera=0;
+   arregloGanador.forEach(combinacion =>{
+           
+    if(
+    (arreglo[combinacion[0][0]][combinacion[0][1]]=='X' && arreglo[combinacion[1][0]][combinacion[1][1]]=='X' && arreglo[combinacion[2][0]][combinacion[2][1]]=='X') 
+    || 
+    (arreglo[combinacion[0][0]][combinacion[0][1]]=='Y' && arreglo[combinacion[1][0]][combinacion[1][1]]=='Y' && arreglo[combinacion[2][0]][combinacion[2][1]]=='Y')
+    )
+        {
+            bandera=true;
+        } 
+   }); 
+   return bandera;
+}
